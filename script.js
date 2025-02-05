4fe748b66c0f068271933f10388994fc20c2da65
Victor Vang <victorvang456@gmail.com>
2025-02-04 22:43:38 -0600

    Added gui and click handler

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function Gameboard() {
             console.log('Invalid move!');
             return false;
         }
-       
+
 
     }
 
@@ -66,13 +66,14 @@ function GameController(
 
     const switchTurnPlayer = (checkMove) => {
         // Only switch turn player if the move was valid; otherwise turn player stays the same until valid move is made
-        if(checkMove) {
+        if (checkMove) {
             activePlayer = activePlayer === players[0] ? players[1] : players[0];
         }
-        
+
     };
 
     const getActivePlayer = () => activePlayer;
+   
 
     const printRound = () => {
         board.printBoard();
@@ -82,15 +83,15 @@ function GameController(
     const playRound = (row, column) => {
         let checkMove = true;
 
-        
+
         checkMove = board.placeMarker(row, column, getActivePlayer().token);
 
         // Placing check for winner and winner message
 
-        
+
         switchTurnPlayer(checkMove);
         printRound();
-        
+
     };
 
     // Start of game
@@ -98,9 +99,58 @@ function GameController(
 
     return {
         playRound,
-        getActivePlayer
+        getActivePlayer,
+        getBoard: board.getBoard
     };
 };
 
+function ScreenController() {
+    const game = GameController();
+    const playerTurnDiv = document.querySelector('.turn');
+    const boardDiv = document.querySelector('.board');
 
-const game = GameController();
+    const updateScreen = () => {
+
+
+        // Reset boarddiv content
+        boardDiv.textContent = "";
+    
+        // Display turn player on screen
+        const board = game.getBoard();
+        const activePlayer = game.getActivePlayer();
+    
+        playerTurnDiv.textContent = `${activePlayer.name}'s turn...`
+    
+    
+        // Render board on screen
+        board.forEach((row, rowIndex) => {
+            row.forEach((token, index) => {
+                const cellButton = document.createElement('button');
+                cellButton.classList.add('cell');
+                cellButton.dataset.column = index;
+                cellButton.dataset.rowNum = rowIndex;
+                cellButton.textContent = token;
+                boardDiv.appendChild(cellButton);
+            })
+        })  
+    }
+
+    // Event listener for the board
+    function clickHandlerBoard(event) {
+        const selectedColumn = event.target.dataset.column;
+        const selectedRow = event.target.dataset.rowNum;
+
+        if (!selectedColumn && !selectedRow) return;
+        game.playRound(selectedRow,selectedColumn);
+        
+        updateScreen();
+    }
+
+    boardDiv.addEventListener("click", clickHandlerBoard);
+
+    // Initial render
+    updateScreen();
+
+}
+
+ScreenController();
